6c4014dea75013dd0e5ade9798339b12d6f73373
dlelo <akinyidonattah@gmail.com>
2019-06-13 10:47:46 +0300

    add view

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello world ');
+const express = require('express');
+const path = require('path');
+
+// init app
+const app = express();
+
+//start server
+const port = 2222;
+app.listen(port, () => console.log('Tutors app server listening on port ' +port+'!') )
+
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+//home route
+app.get('/', function(req, res){
+    res.render('index');
+ });
